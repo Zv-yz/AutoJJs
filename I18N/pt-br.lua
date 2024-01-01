45f82e07eebfce25e99ddad1da15fe916d98b375
Zv_yz <zvyzemail@gmail.com>
2024-01-01 01:12:26 -0300

    [+] I18N\pt-br.lua

---
+++ I18N/pt-br.lua
@@ -0,0 +1,29 @@
+return {
+	Conjunction = 'E';
+	
+	Units = {
+		[0] = 'ZERO', 'UM', 'DOIS', 'TRÊS', 'QUATRO', 'CINCO', 'SEIS', 'SETE', 'OITO', 'NOVE'
+	};
+
+	Special = {
+		[10] = 'DEZ', [11] = 'ONZE', [12] = 'DOZE', [13] = 'TREZE', [14] = 'QUATORZE',
+		[15] = 'QUINZE',
+		[16] = 'DEZESSEIS', [17] = 'DEZESSETE', [18] = 'DEZOITO', [19] = 'DEZENOVE',
+	};
+
+	Tens = {
+		[2] = 'VINTE', [3] = 'TRINTA', [4] = 'QUARENTA', [5] = 'CINQUENTA',
+		[6] = 'SESSENTA', [7] = 'SETENTA', [8] = 'OITENTA', [9] = 'NOVENTA',
+	};
+	
+	Hundred = 'CEM';
+
+	Hundreds = {
+		[1] = 'CENTO', [2] = 'DUZENTOS', [3] = 'TREZENTOS', [4] = 'QUATROCENTOS', [5] = 'QUINHENTOS',
+		[6] = 'SEISCENTOS', [7] = 'SETECENTOS', [8] = 'OITOCENTOS', [9] = 'NOVECENTOS',
+	};
+
+	Thousands = {
+		[1] = 'MIL', [2] = 'MILHÕES', [3] = 'BILHÕES',
+	};
+}
