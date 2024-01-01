ddc46d3bed11032f0dc434fd8072b8c96ae07ab2
Zv_yz <zvyzemail@gmail.com>
2024-01-01 01:10:00 -0300

    [*] Modules\RemoteChat (works with new chat system)

--- Modules/RemoteChat.lua
+++ Modules/RemoteChat.lua
@@ -1,5 +1,5 @@
 local RemoteChat = {};
---local Data = {}
+local TextChatService = game:GetService('TextChatService')
 
 local WC = game.WaitForChild
 local FFC = game.FindFirstChild
@@ -7,30 +7,28 @@ local FFC = game.FindFirstChild
 local Player = game:GetService('Players').LocalPlayer
 local PlayerGui = WC(Player, 'PlayerGui', 95)
 
+local TypeChat = {
+	[Enum.ChatVersion.LegacyChatService] = function(M)
+		local ChatUI = WC(PlayerGui, 'Chat', 95)
+		local ChatFrame = WC(ChatUI, 'Frame', 95)
+		local CBPF = WC(ChatFrame, 'ChatBarParentFrame', 95)
+		local _FRAME = WC(CBPF, 'Frame', 95)
+		local BF = WC(_FRAME, 'BoxFrame', 95)
+		local Frame = WC(BF, 'Frame', 95)
 
--- local function getRemote()
--- 	local ReplicatedStorage = game:GetService('ReplicatedStorage')
--- 	local DefaultRobloxChatEvents = FFC(ReplicatedStorage, 'DefaultChatSystemChatEvents')
+		local ChatBar = FFC(Frame, 'ChatBar', 95)
 
-
--- 	if DefaultRobloxChatEvents and FFC(DefaultRobloxChatEvents, 'SayMessageRequest') then
--- 		Data.Remote = FFC(DefaultRobloxChatEvents, 'SayMessageRequest');
--- 	end
--- end
+		ChatBar:CaptureFocus()
+		ChatBar.Text = M;
+		ChatBar:ReleaseFocus(true)
+	end,
+	[Enum.ChatVersion.TextChatService] = function(M)
+		TextChatService.TextChannels.RBXGeneral:SendAsync(M)
+	end,
+}
 
 function RemoteChat:Send(Message)
-	local ChatUI = WC(PlayerGui, 'Chat', 95)
-	local ChatFrame = WC(ChatUI, 'Frame', 95)
-	local CBPF = WC(ChatFrame, 'ChatBarParentFrame', 95)
-	local _FRAME = WC(CBPF, 'Frame', 95)
-	local BF = WC(_FRAME, 'BoxFrame', 95)
-	local Frame = WC(BF, 'Frame', 95)
-
-	local ChatBar = FFC(Frame, 'ChatBar', 95)
-
-	ChatBar:CaptureFocus()
-	ChatBar.Text = Message
-	ChatBar:ReleaseFocus(true)
+	pcall(TypeChat[TextChatService.ChatVersion], Message)
 end
 
 return RemoteChat
