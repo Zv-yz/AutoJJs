3be01efc6baffd4881631435f0b55edcd28aa5d9
Zv_yz <52023947+Zv-yz@users.noreply.github.com>
2024-02-03 14:32:42 -0300

    [+] I18N/en-us-dev.lua

---
+++ I18N/en-us-dev.lua
@@ -0,0 +1,29 @@
+return {
+	Conjunction = '';
+	
+	Units = {
+		[0] = 'ZERO', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE'
+	};
+
+	Special = {
+		[10] = 'TEN', [11] = 'ELEVEN', [12] = 'TWELVE', [13] = 'THIRTEEN', [14] = 'FOURTEEN',
+		[15] = 'FIFTEEN',
+		[16] = 'SIXTEEN', [17] = 'SEVENTEEN', [18] = 'EIGHTEEN', [19] = 'NINETEEN',
+	};
+
+	Tens = {
+		[2] = 'TWENTY', [3] = 'THIRTY', [4] = 'FORTY', [5] = 'FIFTY',
+		[6] = 'SIXTY', [7] = 'SEVENTY', [8] = 'EIGHTY', [9] = 'NINETY',
+	};
+	
+	Hundred = 'ONE HUNDRED';
+
+	Hundreds = {
+		[1] = 'ONE HUNDRED', [2] = 'TWO HUNDRED', [3] = 'THREE HUNDRED', [4] = 'FOUR HUNDRED', [5] = 'FIVE HUNDRED',
+		[6] = 'SIX HUNDRED', [7] = 'SEVEN HUNDRED', [8] = 'EIGHT HUNDRED', [9] = 'NINE HUNDRED',
+	};
+
+	Thousands = {
+		[1] = 'THOUSAND', [2] = 'MILLION', [3] = 'BILLION',
+	};
+}
