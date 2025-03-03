393d21da2cabbba4e3e8ca1965abe380f212e3cb
Zv_yz <zvyzemail@gmail.com>
2025-03-03 16:02:50 -0300

    [Modules/RemoteChat.lua]: Warning about legacy chat. `https://devforum.roblox.com/t/migrate-to-textchatservice-removing-support-for-legacy-chat-and-custom-chat-systems/3237100`

--- Modules/RemoteChat.lua
+++ Modules/RemoteChat.lua
@@ -1,34 +1,40 @@
-local RemoteChat = {};
-local TextChatService = game:GetService('TextChatService')
+local RemoteChat = {}
 
 local WC = game.WaitForChild
 local FFC = game.FindFirstChild
 
-local Player = game:GetService('Players').LocalPlayer
-local PlayerGui = WC(Player, 'PlayerGui', 95)
+local TextChatService = game:GetService("TextChatService")
+local Players = game:GetService("Players")
 
-local TypeChat = {
-	[Enum.ChatVersion.LegacyChatService] = function(M)
-		local ChatUI = WC(PlayerGui, 'Chat', 95)
-		local ChatFrame = WC(ChatUI, 'Frame', 95)
-		local CBPF = WC(ChatFrame, 'ChatBarParentFrame', 95)
-		local _FRAME = WC(CBPF, 'Frame', 95)
-		local BF = WC(_FRAME, 'BoxFrame', 95)
-		local Frame = WC(BF, 'Frame', 95)
+local Player = Players.LocalPlayer
+local PlayerGui = WC(Player, "PlayerGui", 95)
 
-		local ChatBar = FFC(Frame, 'ChatBar', 95)
+local Methods = {
+	--> Info: Legacy Chat going to be removed on April 30th, 2025.
+	--> Source: https://devforum.roblox.com/t/migrate-to-textchatservice-removing-support-for-legacy-chat-and-custom-chat-systems/3237100
+	
+	[Enum.ChatVersion.LegacyChatService] = function(Message: string)
+		local ChatUI = WC(PlayerGui, "Chat", 95)
+		local ChatFrame = WC(ChatUI, "Frame", 95)
+		local CBPF = WC(ChatFrame, "ChatBarParentFrame", 95)
+		local _FRAME = WC(CBPF, "Frame", 95)
+		local BF = WC(_FRAME, "BoxFrame", 95)
+		local Frame = WC(BF, "Frame", 95)
+
+		local ChatBar = FFC(Frame, "ChatBar", 95)
 
 		ChatBar:CaptureFocus()
-		ChatBar.Text = M;
+		ChatBar.Text = Message
 		ChatBar:ReleaseFocus(true)
 	end,
-	[Enum.ChatVersion.TextChatService] = function(M)
-		TextChatService.TextChannels.RBXGeneral:SendAsync(M)
+	
+	[Enum.ChatVersion.TextChatService] = function(Message: string)
+		TextChatService.TextChannels.RBXGeneral:SendAsync(Message)
 	end,
 }
 
-function RemoteChat:Send(Message)
-	pcall(TypeChat[TextChatService.ChatVersion], Message)
+function RemoteChat:Send(Message: string)
+	pcall(Methods[TextChatService.ChatVersion], Message)
 end
 
 return RemoteChat
