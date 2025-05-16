6f0489a94569279c62a7d4309270a3a49277c9c2
Zv_yz <zvyzemail@gmail.com>
2025-05-16 16:01:40 -0300

    [Modules/RemoteChat.lua]: Fix Tevez thing

--- Modules/RemoteChat.lua
+++ Modules/RemoteChat.lua
@@ -1,18 +1,30 @@
 local RemoteChat = {}
 
 local TextChatService = game:GetService("TextChatService")
+local Channels = {}
+
 local Methods = {
 	[Enum.ChatVersion.LegacyChatService] = function(Message: string)
-		TextChatService.TextChannels.RBXGeneral:SendAsync(Message)
+		Channels["RBXGeneral"]:SendAsync(Message)
 	end,
 	
 	[Enum.ChatVersion.TextChatService] = function(Message: string)
-		TextChatService.TextChannels.RBXGeneral:SendAsync(Message)
+		Channels["RBXGeneral"]:SendAsync(Message)
 	end,
 }
 
 function RemoteChat:Send(Message: string)
+	if not Channels["RBXGeneral"] then
+		return
+	end
+
 	pcall(Methods[TextChatService.ChatVersion], Message)
+end
+
+for _, Channel in ipairs(TextChatService.TextChannels:GetChildren()) do
+	if Channel:IsA("TextChannel") then
+		Channels[Channel.Name] = Channel
+	end
 end
 
 return RemoteChat
