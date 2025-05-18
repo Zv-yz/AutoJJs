83534c3ac2db8f317fa966bd74cc5acb151c5f8b
Zv_yz <zvyzemail@gmail.com>
2025-05-18 14:20:09 -0300

    [Modules/RemoteChat.lua]: Fix for somes places still using legacy chat (but literally!)

--- Modules/RemoteChat.lua
+++ Modules/RemoteChat.lua
@@ -1,23 +1,43 @@
 local RemoteChat = {}
-
-local TextChatService = game:GetService("TextChatService")
 local Channels = {}
+
+local WC = game.WaitForChild
+local FFC = game.FindFirstChild
+
+local Players = game:GetService("Players")
+local TextChatService = game:GetService("TextChatService")
+
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui", 95)
 
 local Methods = {
 	[Enum.ChatVersion.LegacyChatService] = function(Message: string)
-		Channels["RBXGeneral"]:SendAsync(Message)
+		local ChatUI = PlayerGui:FindFirstChild("Chat")
+
+		if ChatUI then
+			local ChatFrame = WC(ChatUI, "Frame", 95)
+			local CBPF = WC(ChatFrame, "ChatBarParentFrame", 95)
+			
+			local Frame = WC(CBPF, "Frame", 95)
+			local BF = WC(Frame, "BoxFrame", 95)
+
+			local ChatFM = WC(BF, "Frame", 95)
+			local ChatBar = FFC(ChatFM, "ChatBar", 95)
+
+			ChatBar:CaptureFocus()
+			ChatBar.Text = Message
+			ChatBar:ReleaseFocus(true)
+		else
+			Channels["RBXGeneral"]:SendAsync(Message)
+		end
 	end,
-	
+
 	[Enum.ChatVersion.TextChatService] = function(Message: string)
 		Channels["RBXGeneral"]:SendAsync(Message)
 	end,
 }
 
 function RemoteChat:Send(Message: string)
-	if not Channels["RBXGeneral"] then
-		return
-	end
-
 	pcall(Methods[TextChatService.ChatVersion], Message)
 end
 
